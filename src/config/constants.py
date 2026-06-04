37124f209943d3b62b97ab6641f29f8fb12ffb29
admin <admin@example.com>
2026-06-05 03:43:48 +0900

    const

--- src/config/constants.py
+++ src/config/constants.py
@@ -23,6 +23,8 @@ AI_AUDIO_TRANSCRIPTS_TEXT_FILE = "audio_transcript_output_file.txt"
 
 ARCHIVE_MOVIE_DIR = "./archive/movie/"
 ARCHIVE_WAV_DIR = "./archive/wav/"
+ARCHIVE_MOVIE_SCORE_DIR = "./archive/movie_score/"
+ARCHIVE_MOVIE_SCORE_FILE = "movie_score_archive.csv"
 
 LOGS_DIR = "./logs/"
 
@@ -37,26 +39,34 @@ STEP1_WORK_DIR = "./src/prompts/step1/"
 STEP1_AI_RESPONSE_FILE = "step1_ai_response_file.md"
 
 # step 2
-STEP2_GEMINI_API_KEY = "AIzaSyAb886Vqo3qVBZ3t3H4-VROWWVkLmGD9-8"
+STEP2_GEMINI_API_KEY = ""
 STEP2_WORK_DIR = "./src/prompts/step2/"
 STEP2_AI_RESPONSE_FILE = "step2_ai_response_file.json"
 
 # step 3
-STEP3_GEMINI_API_KEY = "AIzaSyCdLI3UeQtCa3xNCW6-MwBeNmPUuRp19WU"
+STEP3_GEMINI_API_KEY = ""
 STEP3_WORK_DIR = "./src/prompts/step3/"
 STEP3_AI_RESPONSE_FILE = "step3_ai_response_file.json"
 
 # step 4
-STEP4_GEMINI_API_KEY = "AIzaSyCkzgDe1FVr8zpTk0LUavRu75oApwxfQbY"
+STEP4_GEMINI_API_KEY = ""
 STEP4_WORK_DIR = "./src/prompts/step4/"
 STEP4_AI_RESPONSE_FILE = "step4_ai_response_file.json"
 
 # step 5
-STEP5_GEMINI_API_KEY = "AIzaSyB6sKuhDCIovHSBju352BvtoDjfqUiMDfY"
+
+STEP5_GEMINI_API_KEY = ""
 STEP5_WORK_DIR = "./src/prompts/step5/"
 STEP5_AI_RESPONSE_FILE = "step5_ai_response_file.json"
 
 # step 6
-STEP6_GEMINI_API_KEY = "AIzaSyAzjzAFykHzurex6Uo1lSCBEhKgQM0xV1s"
+STEP6_GEMINI_API_KEY = ""
 STEP6_WORK_DIR = "./src/prompts/step6/"
 STEP6_AI_RESPONSE_FILE = "step6_ai_response_file.json"
+
+# movie score
+MOVIE_SCORE_GEMINI_API_KEY = ""
+MOVIE_SCORE_WORK_DIR = "./src/prompts/movie_score/"
+MOVIE_SCORE_OUTPUT_DIR = "./data/processed/movie_score/"
+MOVIE_SCORE_OUTPUT_FILE = "movie_score.csv"
+MOVIE_SCORE_REJECTED_FILE = "movie_score_rejected.csv"
